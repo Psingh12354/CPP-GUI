5f5b4caa884400fd8be2ea780faf959691a16d8e
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-10-28 14:43:04 +0530

    Update CohenSutherLand1.cpp

--- CohenSutherLand1.cpp
+++ CohenSutherLand1.cpp
@@ -5,6 +5,7 @@
 #define xmax 500
 #define ymin 100
 #define ymax 370
+enum {ABOVE = 0x8,BELOW = 0x4,RIGHT = 0x2,LEFT = 0x1};
 unsigned int encode(int x,int y)
 {
 unsigned int c=0x0;
@@ -22,49 +23,87 @@ if(x<xmin)
 				c=c|ABOVE;
 return c; 
 }
-
-void check(unsigned c1,unsigned c2) 
+// accept=0 line discarder done=0 further calculation requred if 1 no further calculaion required
+void check(unsigned int code1,unsigned int code2,int x1,int y1,int x2,int y2){
+int accept=0,done=0;
+do{
+if(!(code1|code2)) // If both 0000
 {
-if (!(c1|c2))
-	cout<<"Line is Inside";
-else 
-if (c1&c2) 
-	cout<<"Outside";
-else
-	cout<<"Line is Clipping Candidate";
-cout<<"\n";
+accept=1;done=1; // line added and no further calculation
 }
-
+else if(code1&code2)  //bit wise and operation if pv
+{
+accept=0;done=1; //line discarder further calculation
+}
+else
+{
+float m =(y2-y1)/float (x2-x1),x,y;
+int code=code1?code1:code2;
+if(code & ABOVE) //above
+{
+y=ymax;
+x=x1+(y-y1)/m;
+}
+else if(code & BELOW) //below
+{
+y=ymin;
+x=x1+(y-y1)/m;
+}
+else if(code & LEFT) //left
+{
+x=xmin;
+y=m*(x-x1)+y1;
+}
+else   //right
+{
+x=xmax;
+y=m*(x-x1)+y1;
+}
+//compare the code if equal new value assigned
+if(code==code1){
+x1=x;y1=y;
+code1=encode(x1,y1);
+}
+else
+{
+x2=x;y2=y;
+code2=encode(x2,y2);
+}
+}
+}
+while(!done);
+if(accept)
+{cout<<"Accepted"<<endl;
+line(x1,y1,x2,y2);
+}
+}
 void bin(unsigned int c){
-for(int i=8;i>0;i=i/2) 
+for(int i=8;i>0;i/=2)
 (c&i)?cout<<"1":cout<<"0";
 cout<<endl;
 }
 void main(){
-	
 int gd=DETECT,gm;
 clrscr();
 initgraph(&gd,&gm,"");
 int x1,x2,y1,y2;
 unsigned int code1,code2;
-cout<<"X1,Y1 : ";
+cout<<"Enter Coordinates"<<endl;
+cout<<"X1,Y1: ";
 cin>>x1>>y1;
-cout<<"X2,Y2 : ";
+cout<<"X2,Y2: ";
 cin>>x2>>y2;
+code1=encode(x1,y1);
+code2=encode(x2,y2);
 line(xmin,0,xmin,getmaxy());
 line(xmax,0,xmax,getmaxy());
 line(0,ymin,getmaxx(),ymin);
 line(0,ymax,getmaxx(),ymax);
-code1=encode(x1,y1); //to calculate the region code
-code2=encode(x2,y2);
-line(x1,y1,x2,y2);
-cout<<"Point 1 : ";
+cout<<"Region Code Point 1: ";
 bin(code1);
-cout<<"Point 2 : ";
+cout<<"Region Code Point 2: ";
 bin(code2);
-check(code1,code2,)
-check(code1,code2);
+check(code1,code2,x1,y1,x2,y2);
 getch();
 closegraph();
 }
-
